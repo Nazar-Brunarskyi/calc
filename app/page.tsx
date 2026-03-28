763933c32c0034d0dcf7a510951f1b6e9ba5e5ab
Nazar-Brunarskyi <3nnazar836@gmail.com>
2026-03-29 00:25:13 +0200

    Remove user authentication logic from `Page` component, simplifying the structure by returning `null` instead of redirecting based on user status. This change streamlines the component's functionality and prepares for future authentication enhancements.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { getCurrentUserForPage } from "@/src/utils/server/get-current-user-for-page.util";
-import { redirect } from "next/navigation";
-
 export default async function Page() {
-  const user = await getCurrentUserForPage();
-  if (user === null) {
-    redirect("/login");
-  }
-  redirect("/profile");
+  return null;
 }
