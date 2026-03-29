1068fcea9c22ba41c6c6c63c408517eb87fd61c8
Nazar-Brunarskyi <3nnazar836@gmail.com>
2026-03-29 19:09:54 +0300

    Enhance global error handling by integrating toast notifications for unauthorized access. Implement `showAppToast` in the `useGlobalErrorHandlers` hook to display a warning message when users encounter an unauthorized error, prompting them to sign in again. This improves user feedback during error scenarios.

--- src/hooks/api/_shared/use-global-error-handlers.hook.ts
+++ src/hooks/api/_shared/use-global-error-handlers.hook.ts
@@ -1,17 +1,26 @@
 "use client";
 
 import { APP_UNAUTHORIZED_ERROR_TYPES_ENUM } from "@/src/features/error-handling/enums/error-codes/app-unauthorized-error-codes.enum";
+import { TOASTER_TYPES_ENUM } from "@/src/features/error-handling/enums/snackbars/snackbars.enum";
+import { useShowAppToast } from "@/src/features/error-handling/hooks/use-show-app-toast.hook";
 import { useErrorHandler } from "@/src/hooks/api/_shared/use-error-handler.hook";
 import { useRouter } from "next/navigation";
 
 export const useGlobalErrorHandlers = () => {
   const { replace } = useRouter();
+  const { showAppToast } = useShowAppToast();
 
   const { wrapFunction } = useErrorHandler({
     errorHandlers: [
       {
         error_code: APP_UNAUTHORIZED_ERROR_TYPES_ENUM.APP_LEVEL_UNAUTHORIZED,
         handler: () => {
+          showAppToast({
+            type: TOASTER_TYPES_ENUM.WARNING,
+            title: "You are not authorized.",
+            message: "Please sign in again.",
+            icon: "",
+          });
           replace("/login");
         },
       },
