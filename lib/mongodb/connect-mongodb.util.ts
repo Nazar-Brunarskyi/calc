bf14c7b2fd94beadb88db16afccc5a4cf84d0bc1
Nazar-Brunarskyi <3nnazar836@gmail.com>
2026-03-30 11:01:10 +0300

    Improve MongoDB connection utility by adding null check for MONGODB_URI and enhancing cache handling. This ensures a more robust connection process and prevents potential runtime errors when the URI is not set.

--- lib/mongodb/connect-mongodb.util.ts
+++ lib/mongodb/connect-mongodb.util.ts
@@ -18,11 +18,13 @@ function getCache(): IMongooseGlobalCache {
 
 export async function connectMongoDb(): Promise<typeof mongoose> {
   const uri = process.env.MONGODB_URI;
+
   if (!uri) {
     throw new Error("MONGODB_URI is not set");
   }
 
   const cache = getCache();
+
   if (cache.conn) {
     return cache.conn;
   }
